a694523d55446dcf645ecc29793faf7a6c8c09bf
veerababu marisetti <89135089+veerababu5@users.noreply.github.com>
2022-08-23 21:09:00 +0530

    Update windows-instance-creation.tf

--- devops/terraform/windows-instance-creation.tf
+++ devops/terraform/windows-instance-creation.tf
@@ -6,10 +6,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami             = "ami-090fa75af13c156b4"
-  tags            = {
-  name            = "terraform"
-}    
+  ami = "ami-090fa75af13c156b4"
+  tags = {
+    name = "terraform"
+  }
   instance_type   = "t2.micro"
   key_name        = "terraform"
   security_groups = ["${aws_security_group.allow_rdp.name}"]
@@ -20,7 +20,7 @@ resource "aws_security_group" "allow_rdp" {
   description = "allow rdp traffic"
 
   ingress {
-    from_port = 3389 # by default the linux server listens to TCP port 3389 for RDP
+    from_port = 3389 # by default the windows server listens to TCP port 3389 for RDP
     to_port   = 3389
     protocol  = "tcp"
 
