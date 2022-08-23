2fbb09ec087de5d322dff9dd7772c1939f71fe9d
veerababu marisetti <89135089+veerababu5@users.noreply.github.com>
2022-08-23 21:07:12 +0530

    Update linux-instance-creation.tf

--- devops/terraform/linux-instance-creation.tf
+++ devops/terraform/linux-instance-creation.tf
@@ -6,7 +6,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami             = "ami-090fa75af13c156b4"
+  ami = "ami-090fa75af13c156b4"
+  tags = {
+    name = "terraform"
+  }
   instance_type   = "t2.micro"
   key_name        = "terraform"
   security_groups = ["${aws_security_group.allow_ssh.name}"]
